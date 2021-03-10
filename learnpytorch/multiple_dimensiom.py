5ac40b268c89182124af4b297a3eb686ded77749
xinmindliang <liangxinmind@outlook.com>
2021-03-10 19:33:47 +0800

    practice

---
+++ learnpytorch/multiple_dimensiom.py
@@ -0,0 +1,18 @@
+import torch
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear1 = torch.nn.Linear(8, 6)
+        self.linear2 = torch.nn.Linear(6, 4)
+        self.linear3 = torch.nn.Linear(4, 1)
+        self.activate = torch.nn.ReLU()
+
+    def forward(self, x):
+        x = self.activate(self.linear1(x))
+        x = self.activate(self.linear2(x))
+        x = self.activate(self.linear3(x))
+        return x
+        
+model = Model()
