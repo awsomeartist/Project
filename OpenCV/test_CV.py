4cff188ebee4641bebdb895350a131984484d193
xinmindliang <liangxinmind@outlook.com>
2021-03-16 09:38:49 +0800

    test

--- OpenCV/test_CV.py
+++ OpenCV/test_CV.py
@@ -3,12 +3,26 @@ import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
 #show image by using matplot
-img = imread(r"C:\Users\liang\Pictures\Camera Roll\kimono.jpg")
-plt.imshow(img)
-plt.show()
+# img = imread(r"C:\Users\liang\Pictures\Camera Roll\kimono.jpg")
+# plt.imshow(img)
+# plt.show()
 
 #show image by using opencv
 # kimono = cv.imread(r"C:\Users\liang\Pictures\Camera Roll\kimono.jpg", cv.IMREAD_COLOR)
 # kimono = cv.imread(r"C:\Users\liang\Pictures\Camera Roll\kimono.jpg", cv.IMREAD_GRAYSCALE)
 # cv.imshow('kimono', kimono)
 # cv.waitKey(0)
+
+
+# play videos
+capture = cv.VideoCapture(r'C:\Users\liang\Desktop\Desktops\Videos\masami\ma_section.mp4')
+while True:
+    isTrue,frame = capture.read()
+
+    cv.imshow('Videos', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
